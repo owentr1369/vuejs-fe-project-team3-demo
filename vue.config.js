41e4281a979c66d538af9eb165dc0ceadbd5c3dd
TranTam <tamvaa1306@gmail.com>
2022-07-22 17:18:23 +0700

    node modules

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  devServer:{
-    port: 3200
-  }
-})
+  devServer: {
+    port: 8080,
+  },
+});
